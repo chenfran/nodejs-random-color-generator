6cada017701be3692e6c9adb12b218e7b4e3d171
Franziska Chen <chenfranziska@gmail.com>
2024-05-08 00:10:06 +0200

    shortened the code

--- index.js
+++ index.js
@@ -9,63 +9,18 @@ import randomColor from 'randomcolor'; // return random color in hex code
 // print anything in a random color
 // console.log(chalk.hex(randomColor())('#'));
 
-// print anything in a color that a user give us
-const colorFromUser = argv[2];
 // console.log(colorFromUser); // output: colorFromUser but we want the string to be colored [x]
-// if (colorFromUser === 'red') {
-//   console.log(chalk.red('#'));
-// } else if (colorFromUser === 'blue') {
-//   console.log(chalk.blue('#'));
-// } else if (colorFromUser === 'black') {
-//   console.log(chalk.black('#'));
-// } else if (colorFromUser === 'green') {
-//   console.log(chalk.green('#'));
-// } else if (colorFromUser === 'yellow') {
-//   console.log(chalk.yellow('#'));
-// } else if (colorFromUser === 'magenta') {
-//   console.log(chalk.magenta('#'));
-// } else if (colorFromUser === 'cyan') {
-//   console.log(chalk.cyan('#'));
-// } else if (colorFromUser === 'gray') {
-//   console.log(chalk.gray('#'));
-// } else {
-//   console.log('Color does not exist.');
-// }
 
-// I think I have to declare a variable for the function otherwise I get 2 different random colors below
-const colorFromSystem = randomColor();
-
-// combine everything from above - oi
-if (argv.length < 3) {
-  console.log(chalk.hex(colorFromSystem)(hashtagPattern(colorFromSystem)));
-} else if (colorFromUser === 'red') {
-  console.log(chalk.red(hashtagPattern('#ed2b28')));
-} else if (colorFromUser === 'blue') {
-  console.log(chalk.blue(hashtagPattern('#4cb9fa')));
-} else if (colorFromUser === 'black') {
-  console.log(chalk.black(hashtagPattern('#000000')));
-} else if (colorFromUser === 'green') {
-  console.log(chalk.green(hashtagPattern('#2a3f29')));
-} else if (colorFromUser === 'yellow') {
-  console.log(chalk.yellow(hashtagPattern('#f4cf0f')));
-} else if (colorFromUser === 'magenta') {
-  console.log(chalk.magenta(hashtagPattern('#f653a6')));
-} else if (colorFromUser === 'cyan') {
-  console.log(chalk.cyan(hashtagPattern('#00a2d3')));
-} else if (colorFromUser === 'gray') {
-  console.log(chalk.gray(hashtagPattern('#84857d')));
-} else if (colorFromUser === 'white') {
-  console.log(chalk.white(hashtagPattern('#ffffff')));
-} else {
-  console.log('Color does not exist.');
-}
+const colorFromSystem = randomColor(); // declaring a variable for the function
+const usersLuminosity = argv[3]; // declaring users input for luminosity
+const usersColor = argv[2]; // declaring users input for color
 
 // print the pattern
-function hashtagPattern(colorFromUser) {
+function hashtagPattern(usersColor) {
   const fullOfHashtags = '#'.repeat(31);
   const someHashtags = '#'.repeat(5);
   const noHashtags = ' '.repeat(21);
-  const hexCodeColor = ' '.repeat(7) + colorFromUser + ' '.repeat(7);
+  const hexCodeColor = ' '.repeat(7) + usersColor + ' '.repeat(7);
 
   const pattern = `${fullOfHashtags}
 ${fullOfHashtags}
@@ -80,7 +35,17 @@ ${fullOfHashtags}`;
   return pattern;
 }
 
-hashtagPattern(colorFromUser);
+hashtagPattern(usersColor);
 
-// accept luminosity
-const UsersLumi = argv[3];
+const finalColor = randomColor({
+  luminosity: usersLuminosity,
+  hue: usersColor,
+});
+
+if (argv.length < 3) {
+  console.log(chalk.hex(colorFromSystem)(hashtagPattern(colorFromSystem)));
+} else if (finalColor) {
+  console.log(chalk.hex(finalColor)(hashtagPattern(finalColor)));
+} else {
+  console.log('Color does not exist.'); // I will never get this message because it will always be converted to a hex code?
+}
