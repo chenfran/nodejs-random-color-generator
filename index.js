6bbd9003ab206bd6ab2f11d142fd1750b6ea5e4d
Franziska Chen <chenfranziska@gmail.com>
2024-05-11 08:58:38 +0200

    Simplify code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { argv } from 'node:process'; // store users input
 import chalk from 'chalk'; // import colors to node.js - the package is called 'chalk'
 import randomColor from 'randomcolor'; // return random color in hex code
 
-const colorFromSystem = randomColor(); // declaring a variable for the function
 const usersLuminosity = argv[3]; // declaring users input for luminosity
 const usersColor = argv[2]; // declaring users input for color
 const finalColor = randomColor({
@@ -30,12 +29,4 @@ ${fullOfHashtags}`;
   return pattern;
 }
 
-hashtagPattern(usersColor);
-
-if (argv.length < 3) {
-  console.log(chalk.hex(colorFromSystem)(hashtagPattern(colorFromSystem)));
-} else if (finalColor) {
-  console.log(chalk.hex(finalColor)(hashtagPattern(finalColor)));
-} else {
-  console.log('Color does not exist.'); // I will never get this message because it will always be converted to a hex code?
-}
+console.log(chalk.hex(finalColor)(hashtagPattern(finalColor)));
